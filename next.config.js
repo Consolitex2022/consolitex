711a5022d3b209848834fc17e823243089a1f414
Jose Linares <linz.webdev@gmail.com>
2022-09-07 11:52:38 -0400

    agregar consolitex a la lista de cominios permitidos

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['https://consolitex.org/SISGACI', 'https://consolitex.org', 'https://consolitex.org/img.php', 'consolitex.org', 'http://consolitex.org', "", "localhost", "http://localhost", "http://localhost:3000"],
+  },
 }
 
 module.exports = nextConfig

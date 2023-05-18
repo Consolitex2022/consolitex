650e2354d6d31767eab197bcaf1718da5c5fe562
Jose Linares <linz.webdev@gmail.com>
2023-05-18 11:34:47 -0400

    modulo de imagenes

--- pages/photos/edit/[id].tsx
+++ pages/photos/edit/[id].tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useContext, useEffect } from 'react';
+import { FC, useState, useContext, useEffect, ChangeEvent, useRef } from 'react';
 import { NextPage, GetServerSideProps } from 'next';
 
 import Layout from '../../../components/ui/Layout';
@@ -21,6 +21,7 @@ import { ValidatedUser } from '../..';
 import { red, blue } from '@mui/material/colors';
 
 import Swal from 'sweetalert2';
+import axios from 'axios';
 
 interface Props {
     id: string;
@@ -87,19 +88,154 @@ interface PropsImages {
     id: string;
 }
 const ImagesContainer: FC<PropsImages> = ({ baseUrl, images, carpeta, id }) => {
-    const subirImagenes = () => {
+    const [imagesToUpload, setImagesToUpload] = useState<any>(null);
+    const inputFileRef = useRef<HTMLInputElement>(null);
+    // console.log({ imagesToUpload })
+    const location = `${id}/${carpeta}`
+    // Función para subir las imágenes al endpoint
+    const subirImagenes = async () => {
+        const alerta = await Swal.fire({ title: "¿Deseas subir las imagenes?", showCancelButton: true, showConfirmButton: true, cancelButtonColor: red[500], confirmButtonColor: blue[500] })
 
+        if (alerta.isConfirmed) {
+            try {
+                // URL del endpoint donde se enviarán las imágenes
+                const url = `https://consolitex.org/api/v1/fotos/multiple.php`;
+
+                // Crea una instancia de FormData para almacenar las imágenes y otros datos
+                const formData = new FormData();
+
+                // Convierte el FileList en un array
+                const imagesArray = Array.from(imagesToUpload as FileList);
+                // console.log({ imagesArray })
+                if (imagesArray.length > 1) {
+
+                    let counter = 0;
+                    // Itera sobre los archivos
+                    imagesArray.forEach((image) => {
+                        counter++;
+                        formData.append(`images[${counter}]`, image);
+
+                    });
+                } else if (imagesArray.length === 1) {
+                    formData.append(`images`, imagesArray[0]);
+                }
+
+                // Agrega otros datos necesarios al FormData
+                formData.append('location', location);
+                formData.append('accion', 'subirvarias');
+
+                // Realiza la solicitud HTTP utilizando Axios
+                const response = await axios.post(url, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+                // Procesa la respuesta del servidor
+                switch (response.status) {
+
+                    case 200:
+                        // const responseData = await response.json();
+                        console.log(response);
+                        Swal.fire({
+                            title: "Exito",
+                            text: "Se reemplazo la imagen",
+                            icon: "success",
+                            timer: 2000,
+                            timerProgressBar: true,
+                            showConfirmButton: false,
+                        })
+                        setImagesToUpload(null);
+                        break;
+                    case 204:
+                        Swal.fire({
+                            title: "Error",
+                            text: "Ruta no existe",
+                            icon: "warning",
+                            timer: 2000,
+                            timerProgressBar: true,
+                            showConfirmButton: false,
+                        })
+                        break;
+                    case 400:
+                        Swal.fire({
+                            title: "Error",
+                            text: "No se subieron las imagenes",
+                            icon: "error",
+                            timer: 2000,
+                            timerProgressBar: true,
+                            showConfirmButton: false,
+                        })
+                        break;
+                    default:
+                        Swal.fire({
+                            title: "Error",
+                            text: "Error en el servidor",
+                            icon: "error",
+                            timer: 2000,
+                            timerProgressBar: true,
+                            showConfirmButton: false,
+                        })
+                        break;
+                }
+            } catch (error) {
+                // Manejo de errores
+                console.error(error);
+                // Aquí puedes realizar acciones adicionales en caso de error al subir las imágenes
+            }
+        } else {
+            setImagesToUpload(null);
+        }
+    };
+    // // Función para manejar la selección de imágenes desde el input file
+    // const handleFile = (event: ChangeEvent<HTMLInputElement>) => {
+    //     const selectedImages = event.target.files;
+    //     setImagesToUpload(selectedImages);
+    // };
+
+    // // Función para simular el click en el input-file
+    // const chooseFile = () => {
+    //     // Seleccionar el input del DOM utilizando una referencia
+
+    //     if (inputFileRef.current) {
+    //         // Añadir el listener al input para almacenar las imágenes en el state
+    //         inputFileRef.current.addEventListener('change', handleFile);
+    //         // Ejecutar un click en el input file oculto
+    //         inputFileRef.current.click();
+    //     }
+    // };
+    // Función para manejar la selección de imágenes desde el input file
+    const handleFile = (event: Event & { target: HTMLInputElement }) => {
+        const selectedImages = event.target.files;
+        setImagesToUpload(selectedImages);
+    };
+
+    /**
+     * Funcion para simular el click en el input-file
+     */
+    const chooseFile = () => {
+        // Se selecciona el input del DOM
+        const inputFile: HTMLElement = document.querySelector(`#multiple`) as HTMLElement;
+        // Se añade el listener al input para almacenar las imagenes en el state
+        inputFile.addEventListener('change', (e: Event) => handleFile(e as Event & { target: HTMLInputElement }));
+        // Se ejecuta un click en el input file oculto
+        inputFile.click();
     }
     return (
         <Box sx={styles.mainContainer}>
             <Box sx={{ display: "flex", alignItems: 'center', flexFlow: "row nowrap" }}>
                 <Typography fontWeight="bold" fontSize={20} sx={{ fontFamily: "Plus Jakarta Sans" }}>Carpeta {carpeta}</Typography>
-                <Tooltip title="Subir varias imagenes">
-                    <IconButton color="primary" onClick={subirImagenes}>
+                {!imagesToUpload && (<Tooltip title="Subir varias imagenes">
+                    <IconButton color="primary" onClick={chooseFile}>
                         <PlusIcon />
                     </IconButton>
-                </Tooltip>
-                <input type="file" style={{ display: 'none' }} multiple id='multiple' accept="image/jpg, image/png, image/jpeg, image/webp" />
+                </Tooltip>)}
+                {imagesToUpload && (<Tooltip title="Guardar imagenes">
+                    <IconButton color="success" onClick={subirImagenes}>
+                        <SaveRoundedIcon />
+                    </IconButton>
+                </Tooltip>)}
+                <input type="file" id="multiple" ref={inputFileRef} multiple style={{ display: 'none' }} />
+                {/* <input type="file" style={{ display: 'none' }} multiple id='multiple' accept="image/jpg, image/png, image/jpeg, image/webp" /> */}
             </Box>
             <Typography variant="subtitle2" sx={{ fontFamily: "Plus Jakarta Sans" }}>Imagenes de la carpeta {carpeta}</Typography>
             <Box sx={styles.imageContainer}>
@@ -121,12 +257,12 @@ const SingleImage: FC<SingleImageProps> = ({ baseUrl, imagen, carpeta, id }) => 
     const [loading, setLoading] = useState<boolean>(false);
 
     // En donde se va a guardar la imagen
-    const location = `${id}/${carpeta}/`
+    const location = `${id}/${carpeta}`
 
     /**
      * Funcion para manejar el archivo del input-file al state
      * @param event Evento del input-file
-     */
+    */
     const handleFile = (event: Event & { target: HTMLInputElement },) => {
 
         // Validar que existan imagenes seleccionadas y de cantidad de imagenes
@@ -144,8 +280,8 @@ const SingleImage: FC<SingleImageProps> = ({ baseUrl, imagen, carpeta, id }) => 
         setImageSelected(event.target.files[0])
     }
     /**
- * Funcion para simular el click en el input-file
- */
+     * Funcion para simular el click en el input-file
+     */
     const chooseFile = () => {
         // Se selecciona el input del DOM
         const inputFile: HTMLElement = document.querySelector(`#reemplazar`) as HTMLElement;
@@ -154,6 +290,10 @@ const SingleImage: FC<SingleImageProps> = ({ baseUrl, imagen, carpeta, id }) => 
         // Se ejecuta un click en el input file oculto
         inputFile.click();
     }
+    /**
+     * Funcion para reemplazar una imagen en el servidor de imagenes
+     * @param imagen Nombre de la imagen
+     */
     const reemplazarImagen = async (imagen: string) => {
         const alerta = await Swal.fire({ title: "¿Deseas reemplazar esta imagen?", showCancelButton: true, showConfirmButton: true, cancelButtonColor: red[500], confirmButtonColor: blue[500] })
 
@@ -164,11 +304,11 @@ const SingleImage: FC<SingleImageProps> = ({ baseUrl, imagen, carpeta, id }) => 
             body.append('previous_name', imagen)
             body.append('location', location)
             body.append('accion', 'reemplazar')
-            body.append('codigo', String(id))
+            body.append('id', String(id))
             const options = {
                 method: 'POST',
                 headers: {
-                    "Content-Type": "multipart/form-data"
+                    "Accept": "application/json"
                 },
                 body
             }
@@ -234,8 +374,63 @@ const SingleImage: FC<SingleImageProps> = ({ baseUrl, imagen, carpeta, id }) => 
             setImageSelected(null);
         }
     }
-    const borrarImagen = () => {
+    const borrarImagen = async () => {
+        const alerta = await Swal.fire({ title: "¿Deseas borrar esta imagen?", showCancelButton: true, showConfirmButton: true, cancelButtonColor: red[500], confirmButtonColor: blue[500] })
+        if (alerta.isConfirmed) {
+            const url = `https://consolitex.org/api/v1/fotos/index.php?name=${imagen}&location=${location}`
+            const options = {
+                method: 'DELETE',
+                headers: {
+                    "Accept": "application/json"
+                },
+            }
+            try {
+                const respuesta = await fetch(url, options);
+                switch (respuesta.status) {
+                    case 200:
+                        Swal.fire({
+                            title: "Exito",
+                            text: "Se elimino la imagen exitosamente",
+                            icon: "success",
+                            timer: 2000,
+                            timerProgressBar: true,
+                            showConfirmButton: false,
+                        })
+                        break;
+                    case 400:
+                        Swal.fire({
+                            title: "Error",
+                            text: "No se logro eliminar la imagen",
+                            icon: "error",
+                            timer: 2000,
+                            timerProgressBar: true,
+                            showConfirmButton: false,
+                        })
+                        break;
+                    default:
+                        Swal.fire({
+                            title: "Error",
+                            text: "Ocurrio un error en el servidor",
+                            icon: "error",
+                            timer: 2000,
+                            timerProgressBar: true,
+                            showConfirmButton: false,
+                        })
+                        break;
+                }
+            } catch (error) {
+                console.log(error)
+                Swal.fire({
+                    title: "Error",
+                    text: "No se logro conectar con el servidor, intente nuevamente",
+                    icon: "error",
+                    timer: 2000,
+                    timerProgressBar: true,
+                    showConfirmButton: false,
+                })
+            }
 
+        }
     }
     return (
         <Box sx={{ display: "flex", flexFlow: "column nowrap" }}>
@@ -277,7 +472,8 @@ const styles = {
         width: 300,
         height: 300,
         borderRadius: 3,
-        m: 2
+        m: 2,
+        overflow: "hidden"
     },
     actions: { display: "flex", justifyContent: 'center', alignItems: "center" }
 }
